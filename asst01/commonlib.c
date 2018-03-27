dfa2cd92968f384875db8cf2739d82cf964fb8e0
Ethan <rainshanghsu@qq.com>
2018-03-28 02:49:21 +1100

    refactor the implement of application layer (protocol_ func) and the msg format, allowing to transfer and receive unlimited data

---
+++ asst01/commonlib.c
@@ -0,0 +1,254 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#include "commonlib.h"
+
+static const char *CMD_DATA_DELIMITER = "$";
+static const unsigned LEN_CMD_DATA_DELIMITER = 1;
+static const char *CMD_CONFIRM_SURFIX = "_CONFIRM";
+static const unsigned LEN_CMD_CONFIRM_SURFIX = 8;
+
+void perrorf(const char *format, ...)
+{
+    va_list arg_list;
+    va_start(arg_list, format);
+    vfprintf(stderr, format, arg_list);
+    va_end(arg_list);
+    perror("");
+}
+
+void set_fd_block(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
+}
+
+void set_fd_nonblock(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+void set_fd_async(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETOWN, getpid());
+    fcntl(fd, F_SETFL, flags | O_ASYNC);
+}
+
+void set_fd_sync(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETOWN, getpid());
+    fcntl(fd, F_SETFL, flags & ~O_ASYNC);
+}
+
+const char *get_localhost_ip()
+{
+    char host_name[NI_MAXSERV];
+    if (gethostname(host_name, sizeof(host_name)) == -1)
+    {
+        perror("get_localhost_ip: cannot gethostname\n");
+        exit(EXIT_FAILURE);
+    }
+    struct in_addr **h_addr_list = (struct in_addr **)gethostbyname(host_name)->h_addr_list;
+    return inet_ntoa(*h_addr_list[0]);
+}
+
+void set_socket_timeout(int socket_fd, unsigned seconds)
+{
+    struct timeval timeout;
+    timeout.tv_sec = seconds;
+    timeout.tv_usec = 0;
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    {
+        perror("set_socket_timeout: cannot set receive timeout\n");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    {
+        perror("set_socket_timeout: cannot set send timeout\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void init_sockaddr_in(struct sockaddr_in *sockaddr, const char *ip, int port)
+{
+    memset(sockaddr, 0, sizeof(*sockaddr));
+    (*sockaddr).sin_family = AF_INET;
+    (*sockaddr).sin_addr.s_addr = ip ? inet_addr(ip) : htonl(INADDR_ANY);
+    (*sockaddr).sin_port = htons(port);
+}
+
+/**
+ * protocol is {length of msg}{msg}, in which {length of msg} is an int
+ */
+int protocol_write(int fd, const char *msg, size_t msg_size)
+{
+    uint32_t _msg_size = htonl(msg_size); // in case of value varies on different machine
+    char buf[4 + msg_size];
+    // write int to first 4 bytes
+    buf[0] = (_msg_size >> 24) & 0xFF;
+    buf[1] = (_msg_size >> 16) & 0xFF;
+    buf[2] = (_msg_size >> 8) & 0xFF;
+    buf[3] = _msg_size & 0xFF;
+    // write the remaining
+    memcpy(buf + 4, msg, msg_size);
+    return write(fd, buf, sizeof(buf));
+}
+
+// to read the header, which is the size of coming msg
+static int protocol_read_step_one(int fd)
+{
+    uint32_t _msg_size;
+    int len = read(fd, &_msg_size, 4);
+    if (len > 0) // len == sizeof(uint32_t) == 4
+    {
+        return _msg_size;
+    }
+    return len; // didn't read
+}
+
+// after read a valid size, then read that size of msg
+static char *protocol_read_step_two(int fd, size_t msg_size)
+{
+    char *msg = malloc(msg_size);
+    int len = read(fd, msg, msg_size);
+
+    if (len > 0)
+    {
+        return msg;
+    }
+    else
+    {
+        free(msg);
+        fprintf(stderr, "protocol_read_step_two: read msg error\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+char *protocol_read(int fd, size_t *msg_size)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    bool is_fd_block = !(flags & O_NONBLOCK);
+
+    int len;
+    while (true)
+    {
+        len = protocol_read_step_one(fd);
+        if (len > 0)
+        {
+            *msg_size = len;
+            return protocol_read_step_two(fd, len);
+        }
+        else if (len == 0)
+        {
+            if (is_fd_block)
+            {
+                continue;
+            }
+            else // error
+            {
+                *msg_size = len;
+                return NULL;
+            }
+        }
+        else
+        {
+            if (is_fd_block) // error
+            {
+                *msg_size = len;
+                return NULL;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
+
+bool check_errno()
+{
+    return errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN;
+}
+
+unsigned str_len(const char *p_char)
+{
+    const char *tmp_p_char = p_char;
+    unsigned len = 0;
+    while (*tmp_p_char++)
+    {
+        ++len;
+    }
+    return len;
+}
+
+// {cmd}${data}
+char *generate_msg(const char *cmd, const char *data, size_t data_size, size_t *msg_size)
+{
+    unsigned len_cmd = str_len(cmd);
+    *msg_size = len_cmd + LEN_CMD_DATA_DELIMITER + data_size;
+    char *msg = malloc(*msg_size);
+
+    memcpy(msg, cmd, len_cmd);
+    memcpy(msg + len_cmd, CMD_DATA_DELIMITER, LEN_CMD_DATA_DELIMITER);
+    memcpy(msg + len_cmd + LEN_CMD_DATA_DELIMITER, data, data_size);
+
+    return msg;
+}
+
+char **parse_msg(const char *msg, size_t msg_size, size_t *data_size)
+{
+    char *ptr_delimiter = strstr(msg, CMD_DATA_DELIMITER);
+    if (!ptr_delimiter)
+    {
+        return NULL;
+    }
+    char **cmd_data = malloc(sizeof(char *) * 2);
+
+    unsigned len_cmd = ptr_delimiter - msg;
+    cmd_data[0] = malloc(len_cmd);
+    memcpy(cmd_data[0], msg, len_cmd);
+
+    *data_size = msg_size - len_cmd - LEN_CMD_DATA_DELIMITER;
+    cmd_data[1] = malloc(*data_size);
+    ptr_delimiter += LEN_CMD_DATA_DELIMITER;
+    memcpy(cmd_data[1], ptr_delimiter, *data_size);
+
+    return cmd_data;
+}
+
+/**
+ * generate a confirm(reply) cmd of one cmd, {$original_cmd}MSG_CONFIRM_SURFIX
+ * 
+ * remember to free
+ */
+char *generate_confirm_cmd(const char *cmd)
+{
+    unsigned len_cmd = str_len(cmd);
+    unsigned len_confirm_cmd = len_cmd + LEN_CMD_CONFIRM_SURFIX;
+    char *confirm_cmd = malloc(len_confirm_cmd);
+    memcpy(confirm_cmd, cmd, len_cmd);
+    memcpy(confirm_cmd + len_cmd, CMD_CONFIRM_SURFIX, LEN_CMD_CONFIRM_SURFIX);
+    return confirm_cmd;
+}
+
+/**
+ * check {cmd_to_check} is the confirm message of {cmd}, {cmd_to_check} = {cmd}MSG_CONFIRM_SURFIX
+ */
+bool is_confirm_cmd(const char *cmd_to_check, const char *cmd)
+{
+    char *expected_msg = generate_confirm_cmd(cmd);
+    bool b = !strcmp(expected_msg, cmd_to_check);
+    free(expected_msg);
+    return b;
+}
--- asst01/commonlib.h
+++ asst01/commonlib.h
@@ -1,312 +1,56 @@
 #ifndef _COMMON_LIB_H
 #define _COMMON_LIB_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <errno.h>
-#include <sys/time.h>
 
-#define SOCKET_TIMEOUT 10
-#define MSG_CONFIRM_SURFIX "_CONFIRM"
-#define LEN_MSG_CONFIRM_SURFIX 8
-#define CLIENT_MSG_REGISTER "CLIENT_MSG_REGISTER"
-#define CLIENT_MSG_EXIT "CLIENT_MSG_EXIT"
-#define SERVER_MSG_EXIT_CLIENTS "SERVER_MSG_EXIT_CLIENTS"
-#define SERVER_MSG_BARRIER "SERVER_MSG_BARRIER"
-
-typedef int bool;
+typedef char bool;
 #define true 1
 #define false 0
+
+#define SOCKET_TIMEOUT 10
+
+#define CLIENT_CMD_REGISTER "CLIENT_CMD_REGISTER"
+#define CLIENT_CMD_EXIT "CLIENT_CMD_EXIT"
+#define SERVER_CMD_EXIT_CLIENTS "SERVER_CMD_EXIT_CLIENTS"
+#define SERVER_CMD_BARRIER "SERVER_CMD_BARRIER"
 
 /**
  * Like perror(const char *), don't call it when no errno is set
  */
-void perrorf(const char *format, ...)
-{
-    va_list arg_list;
-    va_start(arg_list, format);
-    vfprintf(stderr, format, arg_list);
-    va_end(arg_list);
-    perror("");
-}
-
-void set_fd_block(int fd)
-{
-    int flags = fcntl(fd, F_GETFL, 0);
-    fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
-}
-
-void set_fd_nonblock(int fd)
-{
-    int flags = fcntl(fd, F_GETFL, 0);
-    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-}
-
-void set_fd_async(int fd)
-{
-    int flags = fcntl(fd, F_GETFL, 0);
-    fcntl(fd, F_SETOWN, getpid());
-    fcntl(fd, F_SETFL, flags | O_ASYNC);
-}
-
-void set_fd_sync(int fd)
-{
-    int flags = fcntl(fd, F_GETFL, 0);
-    fcntl(fd, F_SETOWN, getpid());
-    fcntl(fd, F_SETFL, flags & ~O_ASYNC);
-}
-
-const char *get_localhost_ip()
-{
-    char host_name[NI_MAXSERV];
-    if (gethostname(host_name, sizeof(host_name)) == -1)
-    {
-        perror("Gethostname failed\n");
-        exit(EXIT_FAILURE);
-    }
-    struct in_addr **h_addr_list = (struct in_addr **)gethostbyname(host_name)->h_addr_list;
-    return inet_ntoa(*h_addr_list[0]);
-}
-
+void perrorf(const char *format, ...);
+void set_fd_block(int fd);
+void set_fd_nonblock(int fd);
+void set_fd_async(int fd);
+void set_fd_sync(int fd);
+const char *get_localhost_ip();
 /**
  * set socket timeout
  */
-void set_socket_timeout(int socket_fd, unsigned seconds)
-{
-    struct timeval timeout;
-    timeout.tv_sec = seconds;
-    timeout.tv_usec = 0;
-    if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
-    {
-        perror("Server socket cannot set receive timeout\n");
-        exit(EXIT_FAILURE);
-    }
-    if (setsockopt(socket_fd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
-    {
-        perror("Server socket cannot set send timeout\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void init_sockaddr_in(struct sockaddr_in *sockaddr, const char *ip, int port)
-{
-    memset(sockaddr, 0, sizeof(*sockaddr));
-    (*sockaddr).sin_family = AF_INET;
-    (*sockaddr).sin_addr.s_addr = ip ? inet_addr(ip) : htonl(INADDR_ANY);
-    (*sockaddr).sin_port = htons(port);
-}
-
-/**
- * protocol is {$length of data}{$data}, in which {$length of data} is an int
- */
-
-int protocol_write(int fd, const char *data)
-{
-    int len_data = str_len(data);
-    uint32_t _len_data = htonl(len_data); // in case of value varies on different machine
-    char buf[sizeof(uint32_t) + len_data];
-    // write int to first 4 bytes
-    buf[0] = (_len_data >> 24) & 0xFF;
-    buf[1] = (_len_data >> 16) & 0xFF;
-    buf[2] = (_len_data >> 8) & 0xFF;
-    buf[3] = _len_data & 0xFF;
-    // write the remaining
-    memccpy(buf + 4, data, 0, len_data);
-    return write(fd, buf, sizeof(buf));
-}
-
-int protocol_read_once(int fd, char *buf)
-{
-    uint32_t len_data;
-    int len_len = read(fd, &len_data, sizeof(uint32_t));
-    if (len_len > 0) // len_len == sizeof(uint32_t)
-    {
-        read(fd, buf, len_data);
-        buf[len_data] = '\0';
-        return len_data;
-    }
-    return len_len;
-}
-
+void set_socket_timeout(int socket_fd, unsigned seconds);
+void init_sockaddr_in(struct sockaddr_in *sockaddr, const char *ip, int port);
+// use the 2 functions to write/read one message via socket/file/pipe etc.
+int protocol_write(int fd, const char *msg, size_t msg_size);
 /**
  * will check the O_NONBLOCK flag to read in an infinite loop until getting valid messages or error happening
- */
-int protocol_read(int fd, char *buf)
-{
-    int flags = fcntl(fd, F_GETFL, 0);
-    bool is_fd_block = !(flags & O_NONBLOCK);
-
-    int len;
-    while (true)
-    {
-        len = protocol_read_once(fd, buf);
-        if (len > 0)
-        {
-            return len;
-        }
-        else if (len == 0)
-        {
-            if (is_fd_block)
-            {
-                continue;
-            }
-            else // error
-            {
-                return len;
-            }
-        }
-        else
-        {
-            if (is_fd_block) // error
-            {
-                return len;
-            }
-            else
-            {
-                continue;
-            }
-        }
-    }
-}
-
-bool check_errno()
-{
-    return errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN;
-}
-
-/**
- * return a string's length,
- */
-int str_len(const char *p_char)
-{
-    const char *tmp_p_char = p_char;
-    int len = 0;
-    while (*tmp_p_char++)
-    {
-        ++len;
-    }
-    return len;
-}
-
-/**
- * All the message sent from client to server MUST call this function to wrap,
- * otherwise server will deny it
  * 
- * remember to free
+ * return the message and the msg_size
  */
-char *generate_client_msg(int nid, const char *command)
-{
-    int len_msg = str_len(command) + 2;
-    int tmp_nid = nid;
-    if (tmp_nid) // > 0
-    {
-        while (tmp_nid > 0)
-        {
-            tmp_nid /= 10;
-            ++len_msg;
-        }
-    }
-    else
-    {
-        ++len_msg;
-    }
-
-    char *msg = malloc(++len_msg);
-    snprintf(msg,
-             len_msg,
-             "#%d:%s",
-             nid,
-             command);
-    msg[len_msg] = '\0';
-    return msg;
-}
-
+char *protocol_read(int fd, size_t *msg_size);
+bool check_errno();
 /**
- * To parse the message sent from client
- * 
- * @param
- *      msg is the string read from socket, e.g. "#1:CLIENT_MSG_EXIT"
- * @param
- *      the nid in msg, will save in this, e.g. 1
- * 
- * success, return command, e.g. "CLIENT_MSG_EXIT"; else NULL
- * 
- * remember to free
+ * from {p_char} to '\0'
  */
-char *split_client_msg(const char *msg, int *nid)
-{
-    char *p_sharp = strchr(msg, '#');
-    if (!p_sharp)
-    {
-        return NULL;
-    }
-    char *p_colon = strchr(msg, ':');
-    if (!p_colon)
-    {
-        return NULL;
-    }
-    unsigned s_nid_len = p_colon - p_sharp;
-    char s_nid[s_nid_len];
-    unsigned i;
-    for (i = 0; i < s_nid_len; ++i)
-    {
-        ++p_sharp;
-        s_nid[i] = *p_sharp;
-    }
-    *nid = atoi(s_nid);
-
-    char *p_command = p_colon;
-
-    unsigned len_conmmand = 0;
-    while (*p_command++)
-    {
-        ++len_conmmand;
-    }
-    char *command = malloc(len_conmmand);
-    p_command = command;
-    while (*p_colon++)
-    {
-        *p_command = *p_colon;
-        ++p_command;
-    }
-    return command;
-}
-
+unsigned str_len(const char *p_char);
+// use the 2 functions to generate/parse message to between allocator/node
 /**
- * generate a confirm(reply) message of one message, {$original_msg}MSG_CONFIRM_SURFIX
- * 
- * remember to free
+ * return the message and the msg_size
  */
-char *generate_confirm_msg(const char *command)
-{
-    unsigned len_msg = str_len(command);
-    len_msg += LEN_MSG_CONFIRM_SURFIX;
-    char *msg = malloc(++len_msg);
-    snprintf(msg,
-             len_msg,
-             "%s%s",
-             command,
-             MSG_CONFIRM_SURFIX);
-    msg[len_msg] = '\0';
-    return msg;
-}
-
+char *generate_msg(const char *cmd, const char *data, size_t data_size, size_t *msg_size);
 /**
- * check $msg is the confirm message of $command, {$msg} = {$command}MSG_CONFIRM_SURFIX
+ * return [cmd, data] and data_size
  */
-bool is_confirm_msg(const char *msg, const char *command)
-{
-    char *expected_msg = generate_confirm_msg(command);
-    bool b = !strcmp(expected_msg, msg);
-    free(expected_msg);
-    return b;
-}
+char **parse_msg(const char *msg, size_t msg_size, size_t *data_size);
+char *generate_confirm_cmd(const char *cmd);
+bool is_confirm_cmd(const char *cmd_to_check, const char *cmd);
 
 #endif /* _COMMON_LIB_H */
